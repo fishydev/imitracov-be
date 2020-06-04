aa51b52b571ba005b90246b766afed6ae9b66f96
fishydev <huvuhuvuhuvu@gmail.com>
2020-06-04 21:33:48 +0700

    data formatter

---
+++ utils/DataFormatter.js
@@ -0,0 +1,14 @@
+module.exports = fn = data => {
+    return {
+        "nik": data.nik.value,
+        "nama": data.nama.value,
+        "lahir": data.lahir.value,
+        "asalBerangkat": data.asalBerangkat.value,
+        "tglBerangkat": data.tglBerangkat.value,
+        "tglPulang": data.tglPulang.value,
+        "provinsi": data.provinsi.value,
+        "kota": data.kota.value,
+        "kecamatan": data.kecamatan.value,
+        "alamat": data.alamat.value
+    }
+}

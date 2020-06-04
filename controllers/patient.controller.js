def2e870723df75d6f3ab63019f3a067bbb3e867
fishydev <huvuhuvuhuvu@gmail.com>
2020-06-04 21:33:21 +0700

    patient controller

---
+++ controllers/patient.controller.js
@@ -0,0 +1,47 @@
+const DataRepo = require('../repo/DataRepo')
+const DataFormatter = require ('../utils/DataFormatter')
+
+module.exports = {
+    getData: async(req, res) => {
+        try {
+            let patients = await DataRepo.getData(req.query);
+            if(!patients.bindings.length) {
+                return res.status(200).json({
+                    success: true,
+                    status: 200,
+                    data: [],
+                    message: 'Patient not found'
+                })
+            }
+
+            patients = patients.bindings.map((patient) => DataFormatter(patient))
+            
+            if (req.params.nik) {
+
+                let patient = patients.filter((patient) => { return patient.nik == req.params.nik })
+
+                return res.status(200).json({
+                    success: true,
+                    status: 200,
+                    data: patient[0],
+                    message: patient.length ? 'Patient found' : 'Patient not found'
+                })
+            } else {
+                return res.status(200).json({
+                    success: true,
+                    status: 200,
+                    data: patients,
+                    message: 'All patients data retireved'
+                })
+            }
+
+        } catch (err) {
+            return res.status(200).json({
+                success: false,
+                status: 200,
+                data: '',
+                message: `Error: ${err.message}`
+            })
+        }
+    }
+}
